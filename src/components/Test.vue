b9ceae568fececb09f8f1958d91523afacdf742b
wbe <w.b.e@foxmail.com>
2019-04-04 17:01:53 +0800

    router实现页面跳转demo

---
+++ src/components/Test.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="Test">
+      <router-link to="/">页面跳转测试</router-link>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Test'
+}
+</script>
+
+<style scoped>
+
+</style>
